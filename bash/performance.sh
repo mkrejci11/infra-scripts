97dce0f3c191d6ed87f47ef1cab573492706e157
mkrejci11 <mkrejci1@centrum.cz>
2025-05-10 12:00:07 +0200

    Update performance.sh

--- bash/performance.sh
+++ bash/performance.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 SERVERS=("server1.example.com" "server2.example.com")
+
+mkdir -p /var/log/performance_task #check dir
+
 LOG_FILE=/var/log/performance_task/performance_check_$(date +%Y%m%d-%H%M%S).log
 
 for SRV in "${SERVERS[@]}";
@@ -13,15 +16,23 @@ do
         ssh -o ConnectTimeout=20 root@"$SRV" 'bash -s' << 'ENDSSH' | tee -a "$LOG_FILE"
         echo "Hostname: $(hostname)"
         echo "Uptime: $(uptime)"
-        echo "Disk usage:"
+        echo "--- Disk usage: ---"
         df -h
-        echo "Memory usage:"
+        echo "--- Memory usage: ---"
         free -m
+        echo "--- CPU usage: ---"
+        top -b -n1 | head -n 10
+        echo "--- Running systemd services ---"
+        systemctl list-units --type=service --state=running
 ENDSSH
 else
     echo "Nepodarilo se pripojit k $SRV" | tee -a "$LOG_FILE" 
 fi
+
+
     echo "Konec kontroly serveru $SRV" | tee -a "$LOG_FILE"
     echo "" | tee -a "$LOG_FILE"
+
 done
+
 echo "Script dokoncen, vystup ulozen v $LOG_FILE"
