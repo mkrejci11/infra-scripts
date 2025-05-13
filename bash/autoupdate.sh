a08c8888f38e5ef8c62d91e164d84e00876a0299
mkrejci11 <mkrejci1@centrum.cz>
2025-05-13 21:19:10 +0200

    Update autoupdate.sh

--- bash/autoupdate.sh
+++ bash/autoupdate.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
-CHECK_SERVERS=("server1.example.com" "server2.eample.com")
+SERVERS=("server1.example.com" "server2.eample.com")
 
 LOG_DIR=/var/log/autoupdate
 mkdir -p "$LOG_DIR"
 LOG_FILE="LOG_DIR/autoupdate_$(date +%Y%m%d-%H%M%S).log"
+
+#func for connect to servers and perform updates
+update_system() {
+
+}
+
